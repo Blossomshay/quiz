e2ce96995fd17d04dea4a468b3e4afaad2758968
Shania Bute <sbute02@gmail.com>
2019-01-15 23:21:51 +0000

    done

--- js/script.js
+++ js/script.js
@@ -8,7 +8,19 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
+        var q4result = $("#question4").val();
         var totalScore;
-    });
+        totalScore= CalculateQuestion1Score(q1Result);
+        totalScore= totalScore + CalculateQuestion1Score(q2Result)
+         totalScore= totalScore + CalculateQuestion1Score(q3Result)
+         totalScore=totalScore + CalculateQuestionScore(q4Result)
+        console.log ("YEOO")
+        placement(totalScore, $("#name").val
+          }); 
+          
+        if var
+        
+        
+  
 
 });
